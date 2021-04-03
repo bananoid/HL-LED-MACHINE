48843afc2912d808bf7acdc30c4c10eac06a63f8
Giosué Russo <josh@hizen.io>
2021-04-03 18:36:49 +0900

    move baseMessage

--- src/AOYAMA_TREE/COMMON/TrackerFactory.h
+++ src/AOYAMA_TREE/COMMON/TrackerFactory.h
@@ -70,102 +70,373 @@ public:
 
     list<Sequencer *>::iterator it = track->sequencers.begin();
 
-    // First Sequencer
-    sequencer = *it;
+    switch (type)
+    {
+    case DRONE:
+    {
+      // First Sequencer
+      sequencer = *it;
+      sequencer->parameters = params;
 
-    params.stepLenght = 3;
-    params.retrig = 0;
-    params.retrigLFO = 64;
-    params.octave = 3;
-    params.noteCount = 1;
-    params.noteSpread = 2;
-    params.steps = 32;
-    params.events = 8;
-    params.offset = 0;
-    params.chord = 0;
-    params.velocity = -1;
-    params.velocityLFO = 2;
-    sequencer->parameters = params;
+      // Shared parameters
+      params.stepLenght = 3;
+      params.retrig = 0;
+      params.retrigLFO = 64;
+      params.octave = 3;
+      params.noteCount = 1;
+      params.noteSpread = 2;
+      params.steps = 32;
+      params.events = 8;
+      params.offset = 0;
+      params.chord = 0;
+      params.velocity = -1;
+      params.velocityLFO = 2;
 
-    // params.events = 1;
-    // params.stepLenght = 1;
-    // params.noteCount = 1;
+      params.events = 1;
+      params.stepLenght = 1;
+      params.noteCount = 1;
 
-    sequencer->minParameters = params;
+      sequencer->minParameters = params;
 
-    // params.events = 16;
-    // params.stepLenght = 8;
-    // params.noteCount = 7;
-    sequencer->maxParameters = params;
+      params.events = 16;
+      params.stepLenght = 8;
+      params.noteCount = 7;
+      sequencer->maxParameters = params;
 
-    sequencer->randomize();
+      sequencer->randomize();
 
-    // First Sequencer
-    it++;
-    sequencer = *it;
+      // Second Sequencer
+      it++;
+      sequencer = *it;
 
-    params.stepLenght = 3;
-    params.retrig = 0;
-    params.retrigLFO = 23;
-    params.octave = 4;
-    params.noteCount = 1;
-    params.noteSpread = 2;
-    params.steps = 32;
-    params.events = 7;
-    params.offset = 0;
-    params.chord = 0;
-    params.velocity = -1;
-    params.velocityLFO = 4.2;
+      // Shared parameters
+      params.stepLenght = 3;
+      params.retrig = 0;
+      params.retrigLFO = 23;
+      params.octave = 4;
+      params.noteCount = 1;
+      params.noteSpread = 2;
+      params.steps = 32;
+      params.events = 7;
+      params.offset = 0;
+      params.chord = 0;
+      params.velocity = -1;
+      params.velocityLFO = 4.2;
 
-    sequencer->parameters = params;
+      sequencer->parameters = params;
 
-    params.events = 1;
-    params.stepLenght = 3;
-    params.steps = 1;
-    params.noteCount = 1;
-    params.chord = 0;
+      params.events = 1;
+      params.stepLenght = 3;
+      params.steps = 1;
+      params.noteCount = 1;
+      params.chord = 0;
 
-    sequencer->minParameters = params;
+      sequencer->minParameters = params;
 
-    params.events = 16;
-    params.stepLenght = 8;
-    params.steps = 32;
-    params.noteCount = 8;
-    params.chord = 4;
+      params.events = 16;
+      params.stepLenght = 8;
+      params.steps = 32;
+      params.noteCount = 8;
+      params.chord = 4;
 
-    sequencer->maxParameters = params;
+      sequencer->maxParameters = params;
 
-    sequencer->randomize();
+      sequencer->randomize();
 
-    // switch (type)
-    // {
-    // case DRONE:
-    // {
+      break;
+    }
+    case BASS:
+    {
+      // First Sequencer
+      sequencer = *it;
+      sequencer->parameters = params;
 
-    //   break;
-    // }
-    // case BASS:
-    // {
-    //   break;
-    // }
-    // case ARP:
-    // {
+      // Shared parameters
+      params.stepLenght = 3;
+      params.retrig = 0;
+      params.retrigLFO = 64;
+      params.octave = 3;
+      params.noteCount = 1;
+      params.noteSpread = 2;
+      params.steps = 32;
+      params.events = 8;
+      params.offset = 0;
+      params.chord = 0;
+      params.velocity = -1;
+      params.velocityLFO = 2;
 
-    //   break;
-    // }
-    // case CHORD:
-    // {
+      params.events = 1;
+      params.stepLenght = 1;
+      params.noteCount = 1;
 
-    //   break;
-    // }
-    // case PAD:
-    // {
+      sequencer->minParameters = params;
 
-    //   break;
-    // }
+      params.events = 16;
+      params.stepLenght = 8;
+      params.noteCount = 7;
+      sequencer->maxParameters = params;
 
-    // default:
-    //   break;
-    // }
+      sequencer->randomize();
+
+      // Second Sequencer
+      it++;
+      sequencer = *it;
+
+      // Shared parameters
+      params.stepLenght = 3;
+      params.retrig = 0;
+      params.retrigLFO = 23;
+      params.octave = 4;
+      params.noteCount = 1;
+      params.noteSpread = 2;
+      params.steps = 32;
+      params.events = 7;
+      params.offset = 0;
+      params.chord = 0;
+      params.velocity = -1;
+      params.velocityLFO = 4.2;
+
+      sequencer->parameters = params;
+
+      params.events = 1;
+      params.stepLenght = 3;
+      params.steps = 1;
+      params.noteCount = 1;
+      params.chord = 0;
+
+      sequencer->minParameters = params;
+
+      params.events = 16;
+      params.stepLenght = 8;
+      params.steps = 32;
+      params.noteCount = 8;
+      params.chord = 4;
+
+      sequencer->maxParameters = params;
+
+      sequencer->randomize();
+      break;
+    }
+    case ARP:
+    {
+      // First Sequencer
+      sequencer = *it;
+      sequencer->parameters = params;
+
+      // Shared parameters
+      params.stepLenght = 3;
+      params.retrig = 0;
+      params.retrigLFO = 64;
+      params.octave = 3;
+      params.noteCount = 1;
+      params.noteSpread = 2;
+      params.steps = 32;
+      params.events = 8;
+      params.offset = 0;
+      params.chord = 0;
+      params.velocity = -1;
+      params.velocityLFO = 2;
+
+      params.events = 1;
+      params.stepLenght = 1;
+      params.noteCount = 1;
+
+      sequencer->minParameters = params;
+
+      params.events = 16;
+      params.stepLenght = 8;
+      params.noteCount = 7;
+      sequencer->maxParameters = params;
+
+      sequencer->randomize();
+
+      // Second Sequencer
+      it++;
+      sequencer = *it;
+
+      // Shared parameters
+      params.stepLenght = 3;
+      params.retrig = 0;
+      params.retrigLFO = 23;
+      params.octave = 4;
+      params.noteCount = 1;
+      params.noteSpread = 2;
+      params.steps = 32;
+      params.events = 7;
+      params.offset = 0;
+      params.chord = 0;
+      params.velocity = -1;
+      params.velocityLFO = 4.2;
+
+      sequencer->parameters = params;
+
+      params.events = 1;
+      params.stepLenght = 3;
+      params.steps = 1;
+      params.noteCount = 1;
+      params.chord = 0;
+
+      sequencer->minParameters = params;
+
+      params.events = 16;
+      params.stepLenght = 8;
+      params.steps = 32;
+      params.noteCount = 8;
+      params.chord = 4;
+
+      sequencer->maxParameters = params;
+
+      sequencer->randomize();
+      break;
+    }
+    case CHORD:
+    {
+      // First Sequencer
+      sequencer = *it;
+      sequencer->parameters = params;
+
+      // Shared parameters
+      params.stepLenght = 3;
+      params.retrig = 0;
+      params.retrigLFO = 64;
+      params.octave = 3;
+      params.noteCount = 1;
+      params.noteSpread = 2;
+      params.steps = 32;
+      params.events = 8;
+      params.offset = 0;
+      params.chord = 0;
+      params.velocity = -1;
+      params.velocityLFO = 2;
+
+      params.events = 1;
+      params.stepLenght = 1;
+      params.noteCount = 1;
+
+      sequencer->minParameters = params;
+
+      params.events = 16;
+      params.stepLenght = 8;
+      params.noteCount = 7;
+      sequencer->maxParameters = params;
+
+      sequencer->randomize();
+
+      // Second Sequencer
+      it++;
+      sequencer = *it;
+
+      // Shared parameters
+      params.stepLenght = 3;
+      params.retrig = 0;
+      params.retrigLFO = 23;
+      params.octave = 4;
+      params.noteCount = 1;
+      params.noteSpread = 2;
+      params.steps = 32;
+      params.events = 7;
+      params.offset = 0;
+      params.chord = 0;
+      params.velocity = -1;
+      params.velocityLFO = 4.2;
+
+      sequencer->parameters = params;
+
+      params.events = 1;
+      params.stepLenght = 3;
+      params.steps = 1;
+      params.noteCount = 1;
+      params.chord = 0;
+
+      sequencer->minParameters = params;
+
+      params.events = 16;
+      params.stepLenght = 8;
+      params.steps = 32;
+      params.noteCount = 8;
+      params.chord = 4;
+
+      sequencer->maxParameters = params;
+
+      sequencer->randomize();
+      break;
+    }
+    case PAD:
+    { // First Sequencer
+      sequencer = *it;
+      sequencer->parameters = params;
+
+      // Shared parameters
+      params.stepLenght = 3;
+      params.retrig = 0;
+      params.retrigLFO = 64;
+      params.octave = 3;
+      params.noteCount = 1;
+      params.noteSpread = 2;
+      params.steps = 32;
+      params.events = 8;
+      params.offset = 0;
+      params.chord = 0;
+      params.velocity = -1;
+      params.velocityLFO = 2;
+
+      params.events = 1;
+      params.stepLenght = 1;
+      params.noteCount = 1;
+
+      sequencer->minParameters = params;
+
+      params.events = 16;
+      params.stepLenght = 8;
+      params.noteCount = 7;
+      sequencer->maxParameters = params;
+
+      sequencer->randomize();
+
+      // Second Sequencer
+      it++;
+      sequencer = *it;
+
+      // Shared parameters
+      params.stepLenght = 3;
+      params.retrig = 0;
+      params.retrigLFO = 23;
+      params.octave = 4;
+      params.noteCount = 1;
+      params.noteSpread = 2;
+      params.steps = 32;
+      params.events = 7;
+      params.offset = 0;
+      params.chord = 0;
+      params.velocity = -1;
+      params.velocityLFO = 4.2;
+
+      sequencer->parameters = params;
+
+      params.events = 1;
+      params.stepLenght = 3;
+      params.steps = 1;
+      params.noteCount = 1;
+      params.chord = 0;
+
+      sequencer->minParameters = params;
+
+      params.events = 16;
+      params.stepLenght = 8;
+      params.steps = 32;
+      params.noteCount = 8;
+      params.chord = 4;
+
+      sequencer->maxParameters = params;
+
+      sequencer->randomize();
+
+      break;
+    }
+
+    default:
+      Serial.println("Track type not found");
+      break;
+    }
   }
 };
