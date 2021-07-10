8f5aaa023e5787a9545f0b04518f30f4f29df6f6
Giosué Russo <josh@hizen.io>
2021-07-10 17:31:25 +0900

    Memory monitor

--- src/SEQUENCER/main.cpp
+++ src/SEQUENCER/main.cpp
@@ -1,3 +1,4 @@
+
 #include "config.h"
 #include "MainController.h"
 #include <TaskScheduler.h>
@@ -7,6 +8,53 @@
 Scheduler runner;
 MainController *mainController;
 using namespace HLMusicMachine;
+
+// #include "TeensyDebug.h"
+// #pragma GCC optimize("O0")
+
+// #define MEMORY_MONITOR
+
+#ifdef MEMORY_MONITOR
+#include <RamMonitor.h>
+RamMonitor ram;
+uint32_t reporttime;
+
+void report_ram_stat(const char *aname, uint32_t avalue)
+{
+    Serial.print(aname);
+    Serial.print(": ");
+    Serial.print((avalue + 512) / 1024);
+    Serial.print(" Kb (");
+    Serial.print((((float)avalue) / ram.total()) * 100, 1);
+    Serial.println("%)");
+};
+
+void report_ram()
+{
+    bool lowmem;
+    bool crash;
+
+    Serial.println("==== memory report ====");
+
+    report_ram_stat("free", ram.adj_free());
+    report_ram_stat("stack", ram.stack_total());
+    report_ram_stat("heap", ram.heap_total());
+
+    lowmem = ram.warning_lowmem();
+    crash = ram.warning_crash();
+    if (lowmem || crash)
+    {
+        Serial.println();
+
+        if (crash)
+            Serial.println("**warning: stack and heap crash possible");
+        else if (lowmem)
+            Serial.println("**warning: unallocated memory running low");
+    };
+
+    Serial.println();
+};
+#endif
 
 void externalClock()
 {
@@ -28,6 +76,12 @@ void externalStop()
 
 void setup()
 {
+    // debug.begin(SerialUSB1);
+
+#ifdef MEMORY_MONITOR
+    ram.initialize();
+    reporttime = millis();
+#endif
     Serial.begin(115200);
     mainController = new MainController(&runner);
     // serialMIDI.ports[2]->setHandleClock(externalClock);
@@ -37,6 +91,16 @@ void setup()
 
 void loop()
 {
+#ifdef MEMORY_MONITOR
+    uint32_t time = millis();
+    if ((time - reporttime) > 2000)
+    {
+        reporttime = time;
+        report_ram();
+    };
+    ram.run();
+#endif
+
     mainController->update();
     runner.execute();
     // serialMIDI.update();
